8fac40bc260f3a53ded41830e65f007032f28774
Mehedi Hasan <mehedihasan.codes3@gmail.com>
2025-09-28 18:10:38 +0600

    first commit for rechart

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { Activity, Heart } from "lucide-react";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1>Vite + React</h1>
+      <Heart />
+      <Activity />
+    </>
+  );
+}
+
+export default App;
